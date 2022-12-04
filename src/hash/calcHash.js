0814547edca39a2e9570abaf976184c7daefc60e
nonebeer <none.beer@mail.ru>
2022-12-04 18:36:19 +0300

    feat: complete hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import crypto from "crypto";
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const readStream = fs.createReadStream(pathToFile, 'utf-8');
+    let data = '';
+    let hash = '';
+    readStream.on('data', (chunk) => {
+        data += chunk;
+    });
+    readStream.on('error', (error) => {
+        throw error;
+    });
+    readStream.on('end', () => {
+        hash = crypto.createHash('sha256').update(data).digest('hex');
+        console.log(hash);
+    });
 };
 
 await calculateHash();
+
+
+
+
+
