d4faeb2b4d76b86ef0e6c114d7f12a9c7a20f088
nonebeer <none.beer@mail.ru>
2022-12-05 18:34:00 +0300

    feat: complete stream tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readableStream = fs.createReadStream(filePath, 'utf-8');
+    let data = '';
+
+    readableStream.on('data', (chunk) => {
+        data += chunk;
+    })
+    readableStream.on('error', (error) => {
+        throw error;
+    })
+    readableStream.on('end', () => {
+        process.stdout.write(data);
+    })
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const writableStream = fs.createWriteStream(filePath, 'utf-8');
+
+    process.stdin.on('data', (chunk) => {
+        writableStream.write(chunk);
+    });
+
+    process.stdin.on('error', (error) => {
+        writableStream.end();
+        throw error;
+    });
+
+    process.stdin.on('end', () => {
+        writableStream.end();
+    })
 };
 
 await write();
