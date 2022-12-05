22981d050757d16fb1b21207d0492c7a3ebce580
nonebeer <none.beer@mail.ru>
2022-12-05 23:21:07 +0300

    feat: complete child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import cp from "child_process";
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToFile = path.join(__dirname, 'files', 'script.js')
+    const childProcess = cp.fork(pathToFile, args, { silent: true });
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
-spawnChildProcess();
+spawnChildProcess(['hi', 'my', 'name', 'is', 'Slim', 'Shady']);
