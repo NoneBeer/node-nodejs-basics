972b060d11356e7077c6a3f326a19104909b6c3c
nonebeer <none.beer@mail.ru>
2022-12-05 19:22:11 +0300

    feat: complete zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const pathToArchive = path.join(__dirname, 'files', 'archive.gz');
+    const readStream = fs.createReadStream(pathToFile);
+    const writeStream = fs.createWriteStream(pathToArchive);
+    readStream.pipe(zlib.createGzip()).pipe(writeStream)
 };
 
 await compress();
